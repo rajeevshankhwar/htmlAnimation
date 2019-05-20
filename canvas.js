4f55fe33a4d9e5453879fde4a9db4946e6ea9b18
rajeevshankhwar <35623263+rajeevshankhwar@users.noreply.github.com>
2019-05-20 16:49:20 +0530

    Add model View with animation

--- canvas.js
+++ canvas.js
@@ -1,15 +1,52 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// var canvas = document.querySelector('canvas');
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-var timePeriodInMs = 4000;
+// var timePeriodInMs = 4000;
 
-setTimeout(function () {
-    document.getElementById("img").style.display = "none";
-},
-    timePeriodInMs);
+// setTimeout(function () {
+//     // document.getElementById("img").style.display = "none";
+// },
+//     timePeriodInMs);
 
-var c = canvas.getContext('2d');
+
+/**
+ * working code Start
+ */
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+/**
+ * working code End
+ */
+
+// // var c = canvas.getContext('2d');
 // c.fillStyle = 'rgba(255,0,0,0.5)';
 // c.fillRect(100, 100, 100, 100);
 // c.fillStyle = 'rgba(0,0,255,0.5)';
@@ -48,16 +85,16 @@ var c = canvas.getContext('2d');
 // c.arc(200, 200, 30, 0, Math.PI * 2, false);
 // c.strokeStyle = 'red';
 // c.stroke();
-var x = 200;
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    // var x = Math.random() * window.innerWidth;
-    // var y = Math.random() * window.innerHeight;
-    c.beginPath();
-    c.arc(x, 200, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = 'red';
-    c.stroke();
-    x += 1;
-}
-animate();
+// var x = 200;
+// function animate() {
+//     requestAnimationFrame(animate);
+//     c.clearRect(0, 0, innerWidth, innerHeight);
+//     // var x = Math.random() * window.innerWidth;
+//     // var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, 200, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = 'red';
+//     c.stroke();
+//     x += 1;
+// }
+// animate();
